3eadb3d04a38844916751ff2cc385525b5410b1b
NathanNunesCalsonari <130010914+NathanNunesCalsonari@users.noreply.github.com>
2024-06-07 21:01:46 -0300

    Add files via upload

--- oceantech - java/oceantech/oceantech/src/main/java/br/com/fiap/oceantech/OceantechApplication.java
+++ oceantech - java/oceantech/oceantech/src/main/java/br/com/fiap/oceantech/OceantechApplication.java
@@ -2,12 +2,8 @@ package br.com.fiap.oceantech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.com.fiap.oceantech")
-@EnableJpaRepositories(basePackages = "br.com.fiap.oceantech.repository")
 public class OceantechApplication {
 
 	public static void main(String[] args) {
